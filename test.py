614cf77ea26f9ad7c865de997b959bb9fa1106aa
Thomas Viking Christiansson <160872481+ThomasCsson@users.noreply.github.com>
2024-10-19 19:46:04 +0200

    Update

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 list_x = np.linspace(0,10,100)
 list_y = []
+new_list_x = [] 
 
 for x in list_x:
     list_y.append(10-x)
 
 plt.plot(list_x, list_y)
+plt.show()
+
+for x in list_x:
+    new_x = math.sqrt(x)
+    new_list_x.append(new_x)    
+
+plt.plot(new_list_x, list_y)
 plt.show()
